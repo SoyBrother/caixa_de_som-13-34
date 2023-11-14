20f020a8c17c0ea7672c699f7e268e158648fc6d
Felipe Peruceli <105284817+SoyBrother@users.noreply.github.com>
2023-11-14 09:30:32 -0300

    ok

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.querySelector('.tecla_pom').onclick = tocaSom;
 //referência constante listaDeTeclas, buscando todos os botões
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//chamando a função tocaSomPom pelo item "0" da listaDeTecla
+//chamando a função tocaSomPom pela item "0" da listaDeTecla
 listaDeTeclas[0].onclick = TocaSom;
 
 for(let contador = 0;contador < listaDeTeclas.lenght;contador = contador++){
