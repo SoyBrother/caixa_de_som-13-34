0bb2f50c7bd9c1dc7034273cf4faf94690f3437d
Felipe Peruceli <105284817+SoyBrother@users.noreply.github.com>
2023-11-21 09:29:44 -0300

    ok

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 //chamando a função tocaSomPom pela item "0" da listaDeTecla
 listaDeTeclas[0].onclick = TocaSom;
 
-for(let contador = 0;contador < listaDeTeclas.lenght;contador = contador++){
+for(let contador = 0;contador < listaDeTeclas.lenght;contador++){
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1];
     const idAudio = `#som_${efeito}`;
@@ -19,11 +19,11 @@ for(let contador = 0;contador < listaDeTeclas.lenght;contador = contador++){
         tocaSom(idAudio);
     }
     tecla.onkeydown = function(){
-        if(EventCounts.code != 'Tab'){
-        tecla.classList.add('ativa');
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+           tecla.classList.add('ativa');
         }
     }
     tecla.onkeyup = function(){
         tecla.classList.remove('ativa');
     }
-}
+} gfdh
